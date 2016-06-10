0540b2022fccc786c27fab2e9de49fe9679c0170
lucascardoso <lucas.ca.de.araujo@gmail.com>
2016-06-10 15:31:08 -0300

    Adding other buttons on sharing.

--- app/pages/social/social.ts
+++ app/pages/social/social.ts
@@ -1,6 +1,7 @@
 import {Component} from '@angular/core';
 import {SocialSharing} from 'ionic-native';
 import {Page, NavController, Platform} from 'ionic-angular';
+import {Generic} from '../../providers/generic/generic';
 
 /*
   Generated class for the SocialPage page.
@@ -9,17 +10,20 @@ import {Page, NavController, Platform} from 'ionic-angular';
   Ionic pages and navigation.
 */
 @Component({
-  templateUrl: 'build/pages/social/social.html'
+  templateUrl: 'build/pages/social/social.html',
+  providers: [Generic]
 })
 export class SocialPage {
   socialSharing: any;
   platform: any;
+  generic: any;
 
-  constructor(platform: Platform) {
+  constructor(platform: Platform, generic: Generic) {
     platform.ready().then(() => {
       this.socialSharing = SocialSharing;
     });
     this.platform = platform;
+    this.generic = generic;
   }
 
   share(message, subject, file, link) {
@@ -29,8 +33,30 @@ export class SocialPage {
   shareViaTwitter(message, image, link) {
     console.log(":shareViaTwitter:", this.socialSharing);
     this.socialSharing.canShareVia("twitter", message, null, image, link, (result) => {
-      SocialSharing.shareViaTwitter(message, image, link);
+      this.socialSharing.shareViaTwitter(message, image, link);
     }, (error) => {
+      this.generic.showMessage("Twitter", "Para compartilhar com o twitter necessita ter instalado o mesmo no dispositivo");
+      console.error(error);
+    });
+  }
+
+  shareViaFacebook(message, image, link) {
+    console.log(":shareViaFacebook:", this.socialSharing);
+    this.socialSharing.canShareVia("facebook", message, null, image, link, (result) => {
+      this.socialSharing.shareViaFacebook(message, image, link);
+    }, (error) => {
+      this.generic.showMessage("Facebook", "Para compartilhar com o facebook necessita ter instalado o mesmo no dispositivo");
+      console.error(error);
+    });
+  }
+
+  shareViaWhatsApp(message, image, link) {
+    console.log("Generic: ", this.generic);
+    console.log(":shareViaWhatsApp:", this.socialSharing);
+    this.socialSharing.canShareVia("whatsapp", message, null, image, link, (result) => {
+      this.socialSharing.shareViaWhatsApp(message, image, link);
+    }, (error) => {
+      this.generic.showMessage("WhatsApp", "Para compartilhar com o WhatsApp necessita ter instalado o mesmo no dispositivo");
       console.error(error);
     });
   }
